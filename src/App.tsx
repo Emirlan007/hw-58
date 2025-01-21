017c50063d6b12f4a05063e2c1f0436e76368288
Emirlan Kaldarbekov <kaldarbekovemirlan630@gmail.com>
2025-01-21 16:19:45 +0600

    Было внесено изменение в коде в App и Modal так как в App.tsx струкура не соответствало по ТЗ,теперь title можно изменить в App.tsx не как раньше было еще контент можно добавить в App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import './App.css'
+import Modal from "./components/Modal/Modal.tsx";
+import * as React from "react";
 
-const App = () => {
+
+const App: React.FC = () => {
+
+    const [showModal, setShowModal] = React.useState<boolean>(true);
+
     return(
         <>
-
+            <div className="d-flex justify-content-center align-items-center mt-5">
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => setShowModal(!showModal)}
+                >
+                    Open modal
+                </button>
+            </div>
+            <Modal
+                isOpen={showModal}
+                onClose={() => setShowModal(false)}
+                title="Some kinda modal title"
+            >
+                <p>This is modal content</p>
+            </Modal>
         </>
-        )
+    )
 };
 
 export default App
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
 import BackDrop from "../BackDrop/BackDrop.tsx";
 
-interface ModalProps {
+interface ModalProps extends React.PropsWithChildren {
+    title?: string;
     isOpen?: boolean;
     onClose?: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({isOpen = false, onClose}) => {
+const Modal: React.FC<ModalProps> = ({isOpen = false, onClose, children ,title = 'Custom title'}) => {
 return (
     <>
         <BackDrop isOpen={isOpen} onClose={onClose} />
-            <div className="modal show" style={{display: isOpen ? 'block' : 'none'}}>
+            <div className="modal show" style={{display: isOpen ? 'block' : 'none',
+                position: 'fixed',
+                width: '500px',
+                height: 'auto',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5 ">Modal title</h1>
+                            <h1 className="modal-title fs-5 ">{title}</h1>
                             <button type="button" className="btn-close" onClick={onClose}></button>
                         </div>
                         <div className="modal-body">
-                            Modal Content
+                            {children}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-danger" onClick={onClose}>Close</button>
                         </div>
                     </div>
 
